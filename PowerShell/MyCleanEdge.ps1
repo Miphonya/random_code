322ee4c629852430d5a81caa82a6cce58377ab02
Miphonya <85490820+Miphonya@users.noreply.github.com>
2023-05-03 08:36:11 +0200

    Add files via upload

---
+++ PowerShell/MyCleanEdge.ps1
@@ -0,0 +1,7 @@
+﻿Remove-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Edge" -Recurse
+
+$edge = Get-Process -Name msedge -ErrorAction SilentlyContinue
+if ($edge) {
+    Stop-Process -Name msedge
+    Start-Process msedge
+}
