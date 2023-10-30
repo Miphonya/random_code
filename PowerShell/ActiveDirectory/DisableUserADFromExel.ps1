4c7b9b97404394a5b88a1189e2660890a985b305
Menoud Meava <85490820+Miphonya@users.noreply.github.com>
2023-10-30 09:18:22 +0100

    Create DisableUserADFromExel.ps1

---
+++ PowerShell/ActiveDirectory/DisableUserADFromExel.ps1
@@ -0,0 +1,23 @@
+﻿# Import users from a CSV file
+$users = Import-Csv -Path "C:\exemple\exemple.csv"
+
+# Iterate through each user in the imported CSV data
+foreach ($user in $users) {
+    
+    # Extract the "username" field from the current user
+    $Username = $user."username"
+
+    # Get the Active Directory user account based on the extracted username
+    $UserToDisable = Get-ADUser -Identity $Username
+
+    # Check if the user account exists in Active Directory
+    if ($UserToDisable) {
+        # If the user account exists, disable it
+        Disable-ADAccount -Identity $UserToDisable
+        # Output a message indicating that the user has been disabled
+        Write-Host "User disabled: $Username"
+    } else {
+        # If the user account does not exist, output a message indicating it was not found
+        Write-Host "User not found: $Username"
+    }
+}
